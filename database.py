c0626f09f2959ebb35c84f00525402f6cac1ede3
York Ren <me@yuhao.ren>
2026-06-13 03:57:17 +0800

    Create database.py to manage all operations between fastapi app to database.

---
+++ database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "sqlite:///./DATA/to_do_app.sqlite3"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class ToDoList(Base):
+    __tablename__ = "ToDoList"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    property = Column(Integer)
+    completed = Column(Boolean, default=False)
