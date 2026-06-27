b442597d0b4374d776a9bfb571e46f7615d62f84
York Ren <me@yuhao.ren>
2026-06-27 12:47:39 +0800

    Create todos.py to hold classes related to to-do list logic (excluding database mapping models). Implement the ToDosCreate class in this file to handle logic for writing records to the database.

---
+++ todos.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+
+
+class ToDosCreate(BaseModel):
+    title: str = Field(max_length=32)
+    description: str = Field(max_length=64)
+    priority: int = Field(ge=1, le=4)
+    is_completed: bool = Field(default=False)
