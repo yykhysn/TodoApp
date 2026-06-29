1176365388fb679f13ab14c7c45f4655ef5f6cdb
York Ren <me@yuhao.ren>
2026-06-29 16:08:22 +0800

    Refactor `main.py`: first move all existing logic into the router package, then import these fastapi api routers.

--- main.py
+++ main.py
@@ -1,54 +1,10 @@
-from typing import Annotated, Optional
+from fastapi import FastAPI
+import router.auth, router.to_do_app
 
-from fastapi import FastAPI, Depends, Query
 
-from sqlalchemy.orm import Session
-
-from database import SessionLocal, ToDoListTable
-
-from todos import ToDosSchema
 
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-db_dependency = Annotated[Session, Depends(get_db)]
-
-
-@app.get("/debugDatabase")
-def read_all_todolist_table(db: db_dependency):
-    return db.query(ToDoListTable).all()
-
-@app.get("/searchTodos")
-def search_todos(db: db_dependency,
-                 priority: Optional[int] = Query(ge=1, le=4, default=None)):
-    if priority is None:
-        return read_all_todolist_table(db)
-    records = db.query(ToDoListTable).filter(ToDoListTable.priority == priority).all()
-    if records:
-        return records
-    else:
-        return "No such todos"
-
-@app.post("/createTodos")
-def create_todos(db: db_dependency, new_todo: ToDosSchema):
-    new_todo = ToDoListTable(**new_todo.model_dump())
-    db.add(new_todo)
-    db.commit()
-
-@app.put("/updateTodos")
-def update_todos(db: db_dependency, todo_id: int, update_todo: ToDosSchema):
-    record_to_update = db.query(ToDoListTable).get(todo_id)
-    if record_to_update is not None:
-        record_to_update.priority = update_todo.priority
-        record_to_update.title = update_todo.title
-        record_to_update.description = update_todo.description
-        record_to_update.is_completed = update_todo.is_completed
-        db.add(record_to_update)
-        db.commit()
+app.include_router(router.auth.router)
+app.include_router(router.to_do_app.router)
