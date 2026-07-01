c5bfb56713787616bf71d8b9cd407d425c021aff
York Ren <me@yuhao.ren>
2026-07-01 18:32:13 +0800

    Add a POST endpoint /user/register to register a new user.

--- router/auth.py
+++ router/auth.py
@@ -1,10 +1,30 @@
 from fastapi import APIRouter
 
+from schema import UsersSchema
+from database import db_dependency, Users
+
+from passlib.context import CryptContext
+
 
 
 router = APIRouter()
+
+crypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @router.get("/login")
 async def login():
     return {"user": "Login on"}
+
+@router.post("/user/register")
+async def register_user(user_to_register: UsersSchema, db:db_dependency):
+    print(user_to_register)
+    user_to_register = Users(
+        email=user_to_register.email,
+        username=user_to_register.username,
+        first_name=user_to_register.first_name,
+        last_name=user_to_register.last_name,
+        hashed_password=crypt_context.hash(user_to_register.password),
+    )
+    db.add(user_to_register)
+    db.commit()
