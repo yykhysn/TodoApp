64528ccb9af4bf59a54596af7b236f512025f1c3
York Ren <me@yuhao.ren>
2026-07-09 14:01:03 +0800

    Create a utility function `sql_rows_to_dict(rows)` to convert SQL query result rows into a dictionary.

--- utils.py
+++ utils.py
@@ -6,3 +6,6 @@ from starlette import status
 def status_response_error(status_code: int, response_message: str):
     match status_code:
         case 401: raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response_message)
+
+def sql_rows_to_dict(rows):
+    return [row._asdict() for row in rows]
