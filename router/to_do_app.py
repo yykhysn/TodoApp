989fde5866dbcbc2a9330c2269d93010b1d376fe
York Ren <me@yuhao.ren>
2026-07-09 14:05:11 +0800

    Revise the GET endpoint `/searchTodos` so users can only search todo items that belong to themselves. Add a new GET endpoint `/getAllTodos` to fetch all todos associated with the currently logged-in user.

--- router/to_do_app.py
+++ router/to_do_app.py
@@ -3,8 +3,10 @@ from typing import Optional
 from starlette import status
 
 from database import ToDoListTable, Users, db_dependency
-from schema import ToDosSchema
+from config.input_schema import ToDosInputSchema
 from router.auth import user_dependency
+from utils import sql_rows_to_dict
+from config.constants import *
 
 
 
@@ -15,19 +17,27 @@ router = APIRouter()
 def read_all_todolist_table(db: db_dependency):
     return db.query(ToDoListTable).all()
 
-@router.get("/searchTodos")
-def search_todos(db: db_dependency,
-                 priority: Optional[int] = Query(ge=1, le=4, default=None)):
-    if priority is None:
-        return read_all_todolist_table(db)
-    records = db.query(ToDoListTable).filter(ToDoListTable.priority == priority).all()
-    if records:
-        return records
-    else:
-        return "No such todos"
+@router.get("/searchTodos", status_code=status.HTTP_200_OK)
+def search_todos(db: db_dependency, user:user_dependency,
+                 priority: Optional[int] = Query(ge=TODOS_PRIORITY_MIN, le=TODOS_PRIORITY_MAX, default=None),
+                 title: Optional[str] = Query(max_length=TODOS_TITLE_MAX_LEN, default=None),
+                 description: Optional[str] = Query(max_length=TODOS_DESC_MAX_LEN, default=None),
+                 is_completed: Optional[bool] = Query(default=None)):
+    query = (db.query(ToDoListTable.title, ToDoListTable.description, ToDoListTable.priority,
+                      ToDoListTable.is_completed).join(Users, Users.id == ToDoListTable.owner_user_id).
+                 filter(Users.username == user))
+    if priority is not None:
+        query = query.filter(ToDoListTable.priority == priority)
+    if title is not None:
+        query = query.filter(ToDoListTable.title.like(f"%{title}%"))
+    if description is not None:
+        query = query.filter(ToDoListTable.description.like(f"%{description}%"))
+    if is_completed is not None:
+        query = query.filter(ToDoListTable.is_completed == is_completed)
+    return sql_rows_to_dict(query.all())
 
 @router.post("/createTodos", status_code=status.HTTP_201_CREATED)
-def create_todos(user: user_dependency, db: db_dependency, new_todo: ToDosSchema):
+def create_todos(user: user_dependency, db: db_dependency, new_todo: ToDosInputSchema):
     user_id = db.query(Users).filter(Users.username == user).first().id
     new_todo = ToDoListTable(**new_todo.model_dump(), owner_user_id=user_id)
     db.add(new_todo)
@@ -35,7 +45,7 @@ def create_todos(user: user_dependency, db: db_dependency, new_todo: ToDosSchema
     return f"Success! {new_todo.title}'s todo has been created!"
 
 @router.put("/updateTodos")
-def update_todos(db: db_dependency, todo_id: int, update_todo: ToDosSchema):
+def update_todos(db: db_dependency, todo_id: int, update_todo: ToDosInputSchema):
     record_to_update = db.query(ToDoListTable).get(todo_id)
     if record_to_update is not None:
         record_to_update.priority = update_todo.priority
@@ -44,3 +54,10 @@ def update_todos(db: db_dependency, todo_id: int, update_todo: ToDosSchema):
         record_to_update.is_completed = update_todo.is_completed
         db.add(record_to_update)
         db.commit()
+
+@router.get("/getAllTodos", status_code=status.HTTP_200_OK)
+def get_all_todos(db: db_dependency, user: user_dependency):
+    todos_result = (db.query(ToDoListTable.title, ToDoListTable.description, ToDoListTable.priority,
+                            ToDoListTable.is_completed).join(Users, Users.id == ToDoListTable.owner_user_id).
+                    filter(Users.username == user).all())
+    return sql_rows_to_dict(todos_result)
