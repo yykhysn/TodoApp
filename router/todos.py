3acd6179aecbd5b524f24fdd8a898d669cc7e60f
York Ren <me@yuhao.ren>
2026-07-10 18:08:32 +0800

    Add a unified route prefix "/todos" for all endpoints within the `/router/todos` router. Update GET endpoints "/todos/search" "/todos/getAll" to return the `public_uuid` field in responses. Refactor the `/todos/update` endpoint to target todo records by `public_uuid` and validate ownership against the currently authenticated user.

--- router/todos.py
+++ router/todos.py
@@ -4,28 +4,28 @@ from starlette import status
 
 from database import ToDoListTable, Users, db_dependency
 from config.input_schema import ToDosInputSchema
-from router.auth import user_dependency
-from utils import sql_rows_to_dict
+from router.user import user_dependency
+from utils import sql_rows_to_dict, status_response_error
 from config.constants import *
 
 
 
-router = APIRouter()
+router = APIRouter(prefix="/todos", tags=["Todos"])
 
 
 @router.get("/debugDatabase")
 def read_all_todolist_table(db: db_dependency):
     return db.query(ToDoListTable).all()
 
-@router.get("/searchTodos", status_code=status.HTTP_200_OK)
+@router.get("/search", status_code=status.HTTP_200_OK)
 def search_todos(db: db_dependency, user:user_dependency,
                  priority: Optional[int] = Query(ge=TODOS_PRIORITY_MIN, le=TODOS_PRIORITY_MAX, default=None),
                  title: Optional[str] = Query(max_length=TODOS_TITLE_MAX_LEN, default=None),
                  description: Optional[str] = Query(max_length=TODOS_DESC_MAX_LEN, default=None),
                  is_completed: Optional[bool] = Query(default=None)):
     query = (db.query(ToDoListTable.title, ToDoListTable.description, ToDoListTable.priority,
-                      ToDoListTable.is_completed).join(Users, Users.id == ToDoListTable.owner_user_id).
-                 filter(Users.username == user))
+                      ToDoListTable.is_completed, ToDoListTable.public_uuid).
+             join(Users, Users.id == ToDoListTable.owner_user_id).filter(Users.username == user))
     if priority is not None:
         query = query.filter(ToDoListTable.priority == priority)
     if title is not None:
@@ -36,7 +36,7 @@ def search_todos(db: db_dependency, user:user_dependency,
         query = query.filter(ToDoListTable.is_completed == is_completed)
     return sql_rows_to_dict(query.all())
 
-@router.post("/createTodos", status_code=status.HTTP_201_CREATED)
+@router.post("/create", status_code=status.HTTP_201_CREATED)
 def create_todos(user: user_dependency, db: db_dependency, new_todo: ToDosInputSchema):
     user_id = db.query(Users).filter(Users.username == user).first().id
     new_todo = ToDoListTable(**new_todo.model_dump(), owner_user_id=user_id)
@@ -44,20 +44,30 @@ def create_todos(user: user_dependency, db: db_dependency, new_todo: ToDosInputS
     db.commit()
     return f"Success! {new_todo.title}'s todo has been created!"
 
-@router.put("/updateTodos")
-def update_todos(db: db_dependency, todo_id: int, update_todo: ToDosInputSchema):
-    record_to_update = db.query(ToDoListTable).get(todo_id)
-    if record_to_update is not None:
-        record_to_update.priority = update_todo.priority
-        record_to_update.title = update_todo.title
-        record_to_update.description = update_todo.description
-        record_to_update.is_completed = update_todo.is_completed
-        db.add(record_to_update)
-        db.commit()
+@router.put("/update")
+def update_todos(db: db_dependency, user:user_dependency,
+                 public_uuid: str = Query(min_length=32, max_length=32),
+                 priority: Optional[int] = Query(ge=TODOS_PRIORITY_MIN, le=TODOS_PRIORITY_MAX, default=None),
+                 title: Optional[str] = Query(max_length=TODOS_TITLE_MAX_LEN, default=None),
+                 description: Optional[str] = Query(max_length=TODOS_DESC_MAX_LEN, default=None),
+                 is_completed: Optional[bool] = Query(default=None)):
+    record_to_update = (db.query(ToDoListTable).join(Users, Users.id == ToDoListTable.owner_user_id).
+                        filter(Users.username == user, ToDoListTable.public_uuid == public_uuid).first())
+    if record_to_update is None:
+        status_response_error(404, "Todos not found")
+    if priority is not None:
+        record_to_update.priority = priority
+    if title is not None:
+        record_to_update.title = title
+    if description is not None:
+        record_to_update.description = description
+    if is_completed is not None:
+        record_to_update.is_completed = is_completed
+    db.commit()
 
-@router.get("/getAllTodos", status_code=status.HTTP_200_OK)
+@router.get("/getAll", status_code=status.HTTP_200_OK)
 def get_all_todos(db: db_dependency, user: user_dependency):
     todos_result = (db.query(ToDoListTable.title, ToDoListTable.description, ToDoListTable.priority,
-                            ToDoListTable.is_completed).join(Users, Users.id == ToDoListTable.owner_user_id).
-                    filter(Users.username == user).all())
+                            ToDoListTable.is_completed, ToDoListTable.public_uuid).
+                    join(Users, Users.id == ToDoListTable.owner_user_id).filter(Users.username == user).all())
     return sql_rows_to_dict(todos_result)
