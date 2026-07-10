a49aa77dc08d06f5c69f60ba5ec5023f7961c403
York Ren <me@yuhao.ren>
2026-07-10 18:01:45 +0800

    Extend the `status_response_error` function to support 404 Not Found error responses.

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from starlette import status
 def status_response_error(status_code: int, response_message: str):
     match status_code:
         case 401: raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response_message)
+        case 404: raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=response_message)
 
 def sql_rows_to_dict(rows):
     return [row._asdict() for row in rows]
