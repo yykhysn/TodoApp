c09a3164ff9d807d2f6e6b5ccf0502cee426b694
York Ren <me@yuhao.ren>
2026-07-10 18:01:03 +0800

    Define all required column constraints within the SQL ORM mapping classes, and insert sample test data into the DATABASE.

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from fastapi import Depends
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, FetchedValue
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -27,19 +27,20 @@ db_dependency = Annotated[Session, Depends(get_db)]
 class ToDoListTable(Base):
     __tablename__ = "ToDoList"
     id = Column(Integer, primary_key=True)
-    title = Column(String)
-    description = Column(String)
-    priority = Column(Integer)
-    is_completed = Column(Boolean, default=False)
-    owner_user_id = Column(Integer, ForeignKey("Users.id"))
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    priority = Column(Integer, nullable=False)
+    is_completed = Column(Boolean, server_default=FetchedValue(), nullable=False)
+    owner_user_id = Column(Integer, ForeignKey("Users.id"), index=True, nullable=False)
+    public_uuid = Column(String, unique=True, server_default=FetchedValue(), nullable=False)
 
 
 class Users(Base):
     __tablename__ = "Users"
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    hashed_password = Column(String)
-    is_enabled = Column(Boolean, default=True)
+    email = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_enabled = Column(Boolean, default=True, nullable=False)
     id = Column(Integer, primary_key=True)
