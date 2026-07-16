ed097dbcdce42c0fc42101986de8af6e61508b68
York Ren <me@yuhao.ren>
2026-07-16 17:40:39 +0800

    Rename the `UserInputSchema` class to `UserRegisterSchema` dedicated to user registration logic. Create a new `UserUpdateSchema` class to handle post-registration user information modifications, including password and email updates.

--- config/input_schema.py
+++ config/input_schema.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 from config.constants import *
 
@@ -9,9 +10,18 @@ class ToDosInputSchema(BaseModel):
     is_completed: bool = Field(default=TODOS_COMPLETED_DEFAULT)
 
 
-class UsersInputSchema(BaseModel):
+class UsersRegisterSchema(BaseModel):
     username: str = Field(max_length=USERS_USERNAME_MAX_LEN)
     password: str = Field(max_length=USERS_FIRSTNAME_MAX_LEN, min_length=USERS_PASSWORD_MIN_LEN)
     email: str = Field(max_length=USERS_EMAIL_MAX_LEN)
     first_name: str = Field(max_length=USERS_FIRSTNAME_MAX_LEN)
     last_name: str = Field(max_length=USERS_LASTNAME_MAX_LEN)
+
+
+class UsersUpdateSchema(BaseModel):
+    old_password: Optional[str] = Field(max_length=USERS_PASSWORD_MAX_LEN, min_length=USERS_PASSWORD_MIN_LEN, default=None)
+    new_password: Optional[str] = Field(max_length=USERS_PASSWORD_MAX_LEN, min_length=USERS_PASSWORD_MIN_LEN, default=None)
+    email: Optional[str] = Field(max_length=USERS_EMAIL_MAX_LEN, default=None)
+    first_name: Optional[str] = Field(max_length=USERS_FIRSTNAME_MAX_LEN, default=None)
+    last_name: Optional[str] = Field(max_length=USERS_LASTNAME_MAX_LEN, default=None)
+    is_enabled: Optional[bool] = Field(default=None)
