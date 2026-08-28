f10fbba4fc0c3f35f25c4c26997ffd63e3235bfb
York Ren <me@yuhao.ren>
2026-08-28 21:00:20 +0800

    refactor: update user_web.py route to use .html suffix for web page access to enable accessing login page with .html suffix for browser page navigation:
    - Change GET route /user/login to /user/login.html;
    - Add register GET route with path /user/register.html.

--- router/user_web.py
+++ router/user_web.py
@@ -7,6 +7,11 @@ router = APIRouter(prefix="/user", tags=["User Pages"])
 templates = Jinja2Templates(directory="templates/user")
 
 
-@router.get("/login")
+@router.get("/login.html")
 def user_login_page(request: Request):
     return templates.TemplateResponse(request, "login.html")
+
+
+@router.get("/register.html")
+def user_register_page(request: Request):
+    return templates.TemplateResponse(request, "register.html")
