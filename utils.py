f96e4ef395cae0ff391776c20d916d6dde8717fa
York Ren <me@yuhao.ren>
2026-09-02 15:12:10 +0800

    feat: add 400 code support for status_response_error().

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from starlette import status
 
 def status_response_error(status_code: int, response_message: str):
     match status_code:
+        case 400: raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=response_message)
         case 401: raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response_message)
         case 404: raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=response_message)
 
