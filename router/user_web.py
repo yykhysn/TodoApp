6f2ab01a2e5f9b6e579b516048ab907b934a2db2
York Ren <me@yuhao.ren>
2026-09-09 16:13:41 +0800

    refactor(feat): remove Jinja template render endpoints, use static HTML response now instead.

--- router/user_web.py
+++ router/user_web.py
@@ -1,17 +1,6 @@
-from fastapi import Request, APIRouter
+from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
-
 
 router = APIRouter(prefix="/user", tags=["User Pages"])
 
-templates = Jinja2Templates(directory="templates/user")
-
-
-@router.get("/login.html")
-def user_login_page(request: Request):
-    return templates.TemplateResponse(request, "login.html")
-
-
-@router.get("/register.html")
-def user_register_page(request: Request):
-    return templates.TemplateResponse(request, "register.html")
+templates = Jinja2Templates(directory="templates")
