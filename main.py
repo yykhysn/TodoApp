58ec2c1e92452360de154f2f171cf03155d88599
York Ren <me@yuhao.ren>
2026-09-09 15:59:08 +0800

    feat: add GET "/{html_filename:path}.html" endpoint for rendering static html pages.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
-import router.user_api, router.todos_api, router.user_web
-
+import router.user_api, router.todos_api, router.user_web, router.todos_web
+from utils import *
 
 
 app = FastAPI()
@@ -17,3 +20,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/api/health_check")
 async def health_check():
     return {"status": "ok"}
+
+
+@app.get("/{html_filename:path}.html")
+async def render_static_html_pages(html_filename: str):
+    file_address = os.path.join("static/pages", f"{html_filename}.html")
+    if os.path.exists(file_address):
+        return FileResponse(file_address)
+    return status_response_error(404, "Page not found")
