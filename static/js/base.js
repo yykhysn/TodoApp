527d7df36918e097ff69807657ac1753e55545fd
York Ren <me@yuhao.ren>
2026-09-14 15:13:10 +0800

    feat: add logoutUser() for current user logout.

--- static/js/base.js
+++ static/js/base.js
@@ -163,7 +163,7 @@
         });
     }
 
-    // User Register
+    // User Registration
     const userRegisterForm = document.getElementById('registerUserForm');
     if (userRegisterForm) {
         userRegisterForm.addEventListener('submit', async function (event) {
@@ -243,20 +243,8 @@
     //     }
     //     return cookieValue;
     // };
-    //
-    // function logout() {
-    //     // Get all cookies
-    //     const cookies = document.cookie.split(";");
-    //
-    //     // Iterate through all cookies and delete each one
-    //     for (let i = 0; i < cookies.length; i++) {
-    //         const cookie = cookies[i];
-    //         const eqPos = cookie.indexOf("=");
-    //         const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    //         // Set the cookie's expiry date to a past date to delete it
-    //         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
-    //     }
-    //
-    //     // Redirect to the login page
-    //     window.location.href = '/auth/login-page';
-    // };
+
+    // User Logout
+    function logoutUser() {
+        window.location.href = '/user/redirectToLogin.html';
+    }
