8252bd373c6107ad881767b9cc3f5dfbcca80e25
York Ren <me@yuhao.ren>
2026-09-14 14:51:49 +0800

    refactor: apply user_web_dependency to todo_page route. Now user_web_dependency handles webpages authorization.

--- router/todos_web.py
+++ router/todos_web.py
@@ -11,11 +11,6 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/todo.html")
-def todo_page(request: Request, db: db_dependency):
-    user_access_token = request.cookies.get("user_access_token")
-    if user_access_token is not None:
-        current_login_user = validate_user_credential(user_access_token)
-        if current_login_user is not None:
-            current_login_user_lastname = db.query(Users).filter(Users.username == current_login_user).first().last_name
-            return templates.TemplateResponse(request, "/todos/todo.html", {"current_login_user_lastname": current_login_user_lastname})
-    return RedirectResponse("/user/redirectToLogin.html", status_code=status.HTTP_302_FOUND)
+def todo_page(request: Request, user: user_web_dependency, db: db_dependency):
+    login_user_lastname = db.query(Users).filter(Users.username == user).first().last_name
+    return templates.TemplateResponse(request, "/todos/todo.html", {"current_login_user_lastname": login_user_lastname})
