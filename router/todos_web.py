bd249e3531e9473612fbe72967a6b72226bd0f28
York Ren <me@yuhao.ren>
2026-09-16 01:54:52 +0800

    feat: complete "todo.html" to render all user todos.

--- router/todos_web.py
+++ router/todos_web.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, status
 from fastapi.templating import Jinja2Templates
 
 from database import Users, db_dependency
+from router.todos_api import get_all_todos
 from router.user_web import user_web_dependency
 
 
@@ -12,5 +13,8 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/todo.html")
 def todo_page(request: Request, user: user_web_dependency, db: db_dependency):
-    login_user_lastname = db.query(Users).filter(Users.username == user).first().last_name
-    return templates.TemplateResponse(request, "/todos/todo.html", {"current_login_user_lastname": login_user_lastname})
+    current_login_user_lastname = db.query(Users).filter(Users.username == user).first().last_name
+    current_login_user_todos = get_all_todos(db, user)
+    return templates.TemplateResponse(request, "/todos/todo.html",
+                                      {"current_login_user_lastname": current_login_user_lastname,
+                                       "current_login_user_todos": current_login_user_todos})
